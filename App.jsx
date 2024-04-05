42f0f86fa3877bd4799f6ef5b648100b1289474d
Michael Alexandre <157851100+Mikees97@users.noreply.github.com>
2024-04-05 19:34:29 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./styles/global.js";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Card />
+    </>
+  );
+}
+
+export default App;
